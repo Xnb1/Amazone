07fdb7b99209ad171aa73adbe2b137da115aea64
Xnb1 <engrzainabtahir@gmail.com>
2025-06-07 21:45:18 +0400

    .

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
+if (process.env.NODE_ENV != "production") { 
+  require('dotenv').config();
+}
+
 const express = require("express");
 const app = express();
 // const cors = require('cors');
 const mongoose = require('mongoose');
 const session = require("express-session");
+const MongoStore = require('connect-mongo');
 
 const path = require("path");
 const cookieParser = require("cookie-parser");
@@ -17,21 +22,46 @@ const productRouter = require("./routes/product.js");
 
 
 // const initData = require("./init.js");
-MONGO_URL = 'mongodb://127.0.0.1:27017/amazone';
+const dbUrl = process.env.ATLASDB_URL;
 
-main().then(()=> {
-    console.log("connected to DB")
-}).catch((err) => {
-    console.log(err)
+main().then(() => {
+    console.log("connected to database");
+}).catch (err => {
+    console.log(err);
+});
+async function main() {
+    await mongoose.connect(dbUrl);
+}
+
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    crypto: {
+        secret: process.env.SECRET
+    },
+    touchAfter: 24 * 3600,
+}); 
+
+store.on("error", () => {
+    console.log("ERROR IN MONGO SESSION STORE", err);
 });
 
-async function main() {
-    await mongoose.connect(MONGO_URL);
-}
+const sessionOptions = {
+    store,
+    secret : process.env.SECRET,
+    resave : false,
+    saveUninitialized : true,
+    cookie : {
+        expires : Date.now() + 1000*60*60*24*3,
+        maxAge : 1000*60*60*24*3,
+        httpOnly : true
+    }
+};
+
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.use(session(sessionOptions));
 app.use(cookieParser("secretcode"));
 app.use(express.static(path.join(__dirname,"public")));
 app.use(express.urlencoded({extended: true}));
